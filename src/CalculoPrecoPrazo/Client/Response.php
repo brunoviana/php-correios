39c57e7ed2374770214e1d59b2e18ec37f8a7335
Bruno Viana <brunoviana@gmail.com>
2020-05-05 22:48:41 -0300

    Objeto response da requisção de preços e prazos

---
+++ src/CalculoPrecoPrazo/Client/Response.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace BrunoViana\Correios\CalculoPrecoPrazo\Client;
+
+class Response
+{
+    private $dados = [];
+
+    public function __construct(array $dados)
+    {
+        $this->dados = $dados;
+    }
+
+    public function codigo()
+    {
+        return $this->dados['Codigo'];
+    }
+
+    public function valor()
+    {
+        return $this->formataValor($this->dados['Valor']);
+    }
+
+    public function valorSemAdicionais()
+    {
+        return $this->formataValor($this->dados['ValorSemAdicionais']);
+    }
+
+    public function prazoEntrega()
+    {
+        return $this->dados['PrazoEntrega'];
+    }
+
+    public function valorMaoPropria()
+    {
+        return $this->formataValor($this->dados['ValorMaoPropria']);
+    }
+
+    public function valorAvisoRecebimento()
+    {
+        return $this->formataValor($this->dados['ValorAvisoRecebimento']);
+    }
+
+    public function valorValorDeclarado()
+    {
+        return $this->formataValor($this->dados['ValorValorDeclarado']);
+    }
+
+    public function entregaDomiciliar()
+    {
+        // Isso pode acontecer na conversão do retorno para array
+        if (is_array($this->dados['EntregaDomiciliar'])) {
+            return '';
+        }
+        
+        return $this->dados['EntregaDomiciliar'];
+    }
+
+    public function entregaSabado()
+    {
+        // Isso pode acontecer na conversão do retorno para array
+        if (is_array($this->dados['EntregaSabado'])) {
+            return '';
+        }
+        
+        return $this->dados['EntregaSabado'];
+    }
+
+    public function observacao()
+    {
+        // Isso pode acontecer na conversão do retorno para array
+        if (is_array($this->dados['obsFim'])) {
+            return '';
+        }
+
+        return $this->dados['obsFim'];
+    }
+    
+    public function erro()
+    {
+        return $this->dados['Erro'];
+    }
+    
+    public function mensagemErro()
+    {
+        return $this->dados['MsgErro'];
+    }
+
+    private function formataValor($valor)
+    {
+        $valorSoDigitos = preg_replace('/\D/', '', $valor);
+
+        return (float) $valorSoDigitos / 100;
+    }
+}
---
+++ tests/CalculoPrecoPrazo/Client/ResponseTest.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace BrunoViana\Correios\Tests\CalculoPrecoPrazo\Client;
+
+use BrunoViana\Correios\Tests\TestCase;
+use BrunoViana\Correios\CalculoPrecoPrazo\Client\Response;
+
+class ResponseTest extends TestCase
+{
+    public function test_Deve_Retornar_Codigo_Com_Sucesso()
+    {
+        $response = new Response([
+            'Codigo' => '04014'
+        ]);
+
+        $this->assertEquals(
+            '04014',
+            $response->codigo()
+        );
+    }
+
+    public function test_Deve_Retornar_Valor_Com_Sucesso()
+    {
+        $response = new Response([
+            'Valor' => '9,99'
+        ]);
+
+        $this->assertIsFloat($response->valor());
+
+        $this->assertEquals(
+            9.99,
+            $response->valor()
+        );
+    }
+
+    public function test_Deve_Retornar_Prazo_Entrega_Com_Sucesso()
+    {
+        $response = new Response([
+            'PrazoEntrega' => '0'
+        ]);
+
+        $this->assertEquals(
+            '0',
+            $response->prazoEntrega()
+        );
+    }
+
+    public function test_Deve_Retornar_Valor_Sem_Adicionais_Com_Sucesso()
+    {
+        $response = new Response([
+            'ValorSemAdicionais' => '9,99'
+        ]);
+
+        $this->assertIsFloat($response->valorSemAdicionais());
+
+        $this->assertEquals(
+            9.99,
+            $response->valorSemAdicionais()
+        );
+    }
+
+    public function test_Deve_Retornar_Valor_Mao_Propria_Com_Sucesso()
+    {
+        $response = new Response([
+            'ValorMaoPropria' => '9,99'
+        ]);
+
+        $this->assertIsFloat($response->valorMaoPropria());
+
+        $this->assertEquals(
+            9.99,
+            $response->valorMaoPropria()
+        );
+    }
+
+    public function test_Deve_Retornar_Valor_Aviso_Recebimento_Com_Sucesso()
+    {
+        $response = new Response([
+            'ValorAvisoRecebimento' => '9,99'
+        ]);
+
+        $this->assertIsFloat($response->valorAvisoRecebimento());
+
+        $this->assertEquals(
+            9.99,
+            $response->valorAvisoRecebimento()
+        );
+    }
+
+    public function test_Deve_Retornar_Valor_Do_Valor_Declarado_Com_Sucesso()
+    {
+        $response = new Response([
+            'ValorValorDeclarado' => '9,99'
+        ]);
+
+        $this->assertIsFloat($response->valorValorDeclarado());
+
+        $this->assertEquals(
+            9.99,
+            $response->valorValorDeclarado()
+        );
+    }
+
+    public function test_Deve_Retornar_Entrega_Domiciliar_Com_Sucesso()
+    {
+        $response = new Response([
+            'EntregaDomiciliar' => 'S'
+        ]);
+
+        $this->assertEquals(
+            'S',
+            $response->entregaDomiciliar()
+        );
+
+        $response2 = new Response([
+            'EntregaDomiciliar' => ''
+        ]);
+
+        $this->assertEquals(
+            '',
+            $response2->entregaDomiciliar()
+        );
+
+        // pode ficar assim na conversão do array
+        $response3 = new Response([
+            'EntregaDomiciliar' => []
+        ]);
+
+        $this->assertEquals(
+            '',
+            $response3->entregaDomiciliar()
+        );
+    }
+
+    public function test_Deve_Retornar_Entrega_Sabado_Com_Sucesso()
+    {
+        $response = new Response([
+            'EntregaSabado' => 'S'
+        ]);
+
+        $this->assertEquals(
+            'S',
+            $response->entregaSabado()
+        );
+
+        $response2 = new Response([
+            'EntregaSabado' => ''
+        ]);
+
+        $this->assertEquals(
+            '',
+            $response2->entregaSabado()
+        );
+
+        // pode ficar assim na conversão do array
+        $response3 = new Response([
+            'EntregaSabado' => []
+        ]);
+
+        $this->assertEquals(
+            '',
+            $response3->entregaSabado()
+        );
+    }
+
+    public function test_Deve_Retornar_Observacao_Com_Sucesso()
+    {
+        $response = new Response([
+            'obsFim' => 'Nenhuma observação'
+        ]);
+
+        $this->assertEquals(
+            'Nenhuma observação',
+            $response->observacao()
+        );
+
+        // pode ficar assim na conversão do array
+        $response3 = new Response([
+            'obsFim' => []
+        ]);
+
+        $this->assertEquals(
+            '',
+            $response3->observacao()
+        );
+    }
+    
+    public function test_Deve_Retornar_Erro_Com_Sucesso()
+    {
+        $response = new Response([
+            'Erro' => '-888'
+        ]);
+
+        $this->assertEquals(
+            '-888',
+            $response->erro()
+        );
+    }
+    
+    public function test_Deve_Retornar_Mensagem_De_Erro_Com_Sucesso()
+    {
+        $response = new Response([
+            'MsgErro' => 'Não foi encontrada precificação. ERP-008: Dimensoes nao localizadas ou ultrapassam os limites aceitos para este tipo de objeto(-1).'
+        ]);
+
+        $this->assertEquals(
+            'Não foi encontrada precificação. ERP-008: Dimensoes nao localizadas ou ultrapassam os limites aceitos para este tipo de objeto(-1).',
+            $response->mensagemErro()
+        );
+    }
+}
