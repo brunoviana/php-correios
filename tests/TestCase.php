d1bf33a3fed4670051e9d48c60ec84027c5ec6d3
Bruno Viana <brunoviana@gmail.com>
2020-05-05 22:48:01 -0300

    Iniciando projeto

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BrunoViana\Correios\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+abstract class TestCase extends PHPUnitTestCase
+{
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        error_reporting(E_ALL);
+    }
+}
