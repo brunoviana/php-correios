ddcebb390155b75330d85a2febd42e0176cdcd58
Bruno Viana <brunoviana@gmail.com>
2020-05-08 16:26:01 -0300

    Exemplos envelope

---
+++ exemplos/CalculoPrecoPrazo/calculo_envelope_metodos.php
@@ -0,0 +1,41 @@
+<?php
+
+require '../../vendor/autoload.php';
+
+use BrunoViana\Correios\CalculoPrecoPrazo;
+
+$calculador = CalculoPrecoPrazo::calculador();
+$calculador->servicos([
+    '41106',
+])
+            ->itens([
+                [
+                    'quantidade' => 1,
+                    'peso' => 1,
+                    'comprimento' => 31,
+                    'diametro' => 15,
+                ],
+            ])
+            ->usuario('')
+            ->senha('')
+            ->origem('60842-130')
+            ->destino('22775-051')
+            ->formato(CalculoPrecoPrazo::ENVELOPE)
+            ->maoPropria('N')
+            ->valorDeclarado(0)
+            ->avisoRecebimento('N');
+
+$responses = $calculador->calcular();
+
+echo '<pre>';
+
+// Retorna um array onde cada índice é o resultado de um serviço consultado
+foreach ($responses as $response) {
+    if (! $response->sucesso()) {
+        echo '<p>Falha ao consultar os Correios. Status Code: ' . $response->codigoHttp() . '</p>';
+        continue;
+    }
+
+    // Você pode usar os métodos ou transformar tudo em array
+    print_r((array) $response->emArray());
+}
