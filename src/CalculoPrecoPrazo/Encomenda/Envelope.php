1016242e29755e1bac7c1e58fbd937394c160203
Bruno Viana <brunoviana@gmail.com>
2020-05-08 15:55:52 -0300

    TIpo de encomenda Envelope

--- src/CalculoPrecoPrazo/Encomenda/Envelope.php
+++ src/CalculoPrecoPrazo/Encomenda/Envelope.php
@@ -4,18 +4,18 @@ namespace BrunoViana\Correios\CalculoPrecoPrazo\Encomenda;
 
 use BrunoViana\Correios\CalculoPrecoPrazo\Encomenda;
 
-class Rolo extends Encomenda
+class Envelope extends Encomenda
 {
-    const COMPRIMENTO_MINIMO = 18;
+    const COMPRIMENTO_MINIMO = 16;
 
-    const DIAMETRO_MINIMO = 5;
+    const LARGURA_MINIMA = 11;
 
     public function comprimento()
     {
-        $comprimento = array_sum(
+        $comprimento = max(
             array_map(
                 function ($item) {
-                    return $item->comprimento() * $item->quantidade();
+                    return $item->comprimento();
                 },
                 $this->itens
             )
@@ -24,29 +24,29 @@ class Rolo extends Encomenda
         return $comprimento > self::COMPRIMENTO_MINIMO ? $comprimento : self::COMPRIMENTO_MINIMO;
     }
 
-    // Cálculo de rolo/cilindro não usa largura
+    // Cálculo de envelope não usa altura
     public function altura()
     {
         return 0;
     }
 
-    // Cálculo de rolo/cilindro não usa largura
     public function largura()
     {
-        return 0;
-    }
-
-    public function diametro()
-    {
-        $diametro = max(
+        $largura = max(
             array_map(
                 function ($item) {
-                    return $item->diametro();
+                    return $item->largura();
                 },
                 $this->itens
             )
         );
 
-        return $diametro > self::DIAMETRO_MINIMO ? $diametro : self::DIAMETRO_MINIMO;
+        return $largura > self::LARGURA_MINIMA ? $largura : self::LARGURA_MINIMA;
+    }
+
+    // Cálculo de envelope não usa diâmetro
+    public function diametro()
+    {
+        return 0;
     }
 }
