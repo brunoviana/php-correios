2b2cc1c33927311cae61ac853b31dc7f4db568ed
Bruno Viana <brunoviana@gmail.com>
2020-05-07 22:59:32 -0300

    Fazendo código se manter dentro da PSR2

--- src/CalculoPrecoPrazo/Exceptions/ParametroInvalidoException.php
+++ src/CalculoPrecoPrazo/Exceptions/ParametroInvalidoException.php
@@ -9,7 +9,11 @@ class ParametroInvalidoException extends Exception
     public function __construct($message = null, $code = 0, Exception $previous = null)
     {
         if (is_array($message)) {
-            $message = sprintf('Parâmetro passado não é válido: "%s" (o valor "%s" foi informado)', $message[0], $message[1]);
+            $message = sprintf(
+                'Parâmetro passado não é válido: "%s" (o valor "%s" foi informado)',
+                $message[0],
+                $message[1]
+            );
         } else {
             $message = sprintf('Parâmetro passado não é válido: "%s"', $message);
         }
